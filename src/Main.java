411943b6323a076cbf358f0c838e65d1f1cf4020
Ruslanqwd1212 <116277908+Ruslanqwd1212@users.noreply.github.com>
2023-01-27 17:01:53 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayListCreation arrayListCreation = new ArrayListCreation();
+
+        arrayListCreation.sortArrayList();
+    }
+}
